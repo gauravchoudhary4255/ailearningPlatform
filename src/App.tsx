f3ba97fb4264e1acd6f6b2ebb4d9bdfc4e8f73ce
gauravchoudhary4255 <68188206+gauravchoudhary4255@users.noreply.github.com>
2025-08-22 10:14:31 +0530

    Home page Designs Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer"
+import Home from "./components/Home";
 
 function App(){
 return <>
 <BrowserRouter>
 <Header/>
+<Home/>
 <Footer/>
 </BrowserRouter>
 </>
